2a1cf8af1505a16890f73cbc49c0c2af69ecc6d5
Vukasin Gostovic <vukasin@gostovic.me>
2024-01-07 23:23:39 +0100

    dont do shit casts

--- hrotti/websocket_manager.py
+++ hrotti/websocket_manager.py
@@ -9,7 +9,6 @@ async def accept_ws_request(info: BlockInfo, websocket: WebSocket):
     data = await websocket.receive_json()
     try:
         request = RPCRequest(**data)
-        response_dict = handle_request(info, request)
-        await websocket.send_text(json.dumps(response_dict))
+        await websocket.send_json(handle_request(info, request))
     except json.JSONDecodeError:
         await websocket.send_text("Error: Invalid JSON")
