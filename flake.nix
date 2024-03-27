6cca9ac3f7ab588a007060e2141a2274d5dc2c6a
Vukasin Gostovic <vukasin@gostovic.me>
2024-03-27 13:50:39 +0100

    flake devshell ps1

--- flake.nix
+++ flake.nix
@@ -26,6 +26,16 @@
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.myapp ];
           packages = [ pkgs.poetry ];
+
+          shellHook = ''
+            export OLD_PS1="$PS1" # Preserve the original PS1
+            export PS1="nix-shell:hrotti $PS1" # Customize this line as needed
+          '';
+
+          # reset ps1
+          shellExitHook = ''
+            export PS1="$OLD_PS1"
+          '';
         };
       });
 }
